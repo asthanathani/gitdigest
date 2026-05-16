8903bdd631e81ad37c342dc79d887b825865887f
asthanathani <asthanathani321@gmail.com>
2026-05-16 16:33:27 +0530

    update backend URL to production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
     setResult(null);
 
     try {
-      const res = await fetch("http://localhost:3001/analyze", {
+      const res = await fetch("https://gitdigest.onrender.com/analyze", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ repoUrl }),
